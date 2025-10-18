667a95d4007f911f044bcb000495e0736c1c4e98
Felipe <felipbarreto@icloud.com>
2025-10-18 20:05:37 -0300

    feat: bcrypt and nodemon

--- src/controller/user.controllers.js
+++ src/controller/user.controllers.js
@@ -5,8 +5,8 @@ async function createUserController (req, res) {
     const newUser = req.body;
 
     try {
-        const user = userService.createUserService(newUser);
-        res.status(201).send({user})
+        const user = await userService.createUserService(newUser);
+        return res.status(201).send({user})
     } catch (err) {
         return res.status(400).send(err.message);
     }
--- src/service/user.services.js
+++ src/service/user.services.js
@@ -1,8 +1,16 @@
-import userRepositories from "../repositories/user.repositories.js";
+import userRepository from "../repositories/user.repositories.js";
+import bcrypt from "bcrypt"
 
 async function createUserService (newUser) {
-    const user = await userRepositories.createUserRepository(newUser)
+    const foundUser = await userRepository.findUserByEmailRepository(newUser.email)
+    if(foundUser) throw new Error("User already exists!");
 
+    const passHash = await bcrypt.hash(newUser.password,10)
+    const user = await userRepository.createUserRepository({
+        ...newUser, 
+        password: passHash
+    });
+    if(!user) throw new Error("Error creating User");
     return user;
 }
 
