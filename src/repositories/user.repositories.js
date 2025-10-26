c2c79f40ad94d054e7ab110d87c06240b62dd509
Felipe <felipbarreto@icloud.com>
2025-10-26 16:27:52 -0300

    feat: dynamic update query

--- src/repositories/user.repositories.js
+++ src/repositories/user.repositories.js
@@ -1,5 +1,3 @@
-//01 min 30seg AULA 42
-
 import db from '../config/database.js'
 
 db.run(`
@@ -12,11 +10,10 @@ db.run(`
         )
     `)
 
-//06min 48
 function createUserRepository (newUser) {
     const {username, email, password, avatar} = newUser
     return new Promise((resolve, reject) => {
-        db.run(                                 //09MIN 16
+        db.run(                                
             `
                 INSERT INTO users (username, email, password, avatar)
                 VALUES (?, ?, ?, ?)
@@ -53,7 +50,91 @@ function findUserByEmailRepository(email) {
     });
 }
 
+function findUserByIdRepository(id) {
+    return new Promise((resolve, reject) => {
+        db.get(
+            `
+            SELECT id, username, email, avatar
+            FROM users
+            WHERE id = ?
+            `,
+            [id],
+            (err, row) => {
+                if (err) {
+                    reject(err);
+                }else {
+                    resolve(row);
+                }
+            }
+        );
+    });
+}
+
+function findAllUserRepository() {
+    return new Promise((resolve, reject) => {
+        db.all(
+            `
+                SELECT id, username, email, avatar FROM users
+            `, [], (err, rows) => {
+                if(err) {
+                    reject(err)
+                } else {
+                    resolve(rows)
+                }
+            }
+        )
+    })
+}
+
+function updateUserRepository(id, user) {
+    return new Promise((resolve, reject) => {
+        const { username, email, password, avatar } = user
+        const fields = ["username", "email", "password", "avatar"]
+        let query = "UPDATE users SET"
+        const values = []
+
+        fields.forEach((field) => {
+            if(user[field] !== undefined) {
+                query += ` ${field} = ?,`
+                values.push(user[field])
+            }
+        })     
+
+        query = query.slice(0, -1)
+
+        query += " WHERE id = ?"
+        values.push(id)
+
+        db.run(query, values, (err) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve({ ...user, id });
+            }
+        });
+    });
+}
+
+function deleteUserRepository(id) {
+    return new Promise((resolve, reject) => {
+        db.run(`
+                DELETE FROM users
+                WHERE id = ?
+            `, [id], (err) => {
+                if(err) {
+                    reject(err)
+                } else {
+                    resolve({ message: "User deleted successfully", id })
+                }
+            })
+    })
+}
+
 export default {
     createUserRepository, 
-    findUserByEmailRepository
+    findUserByEmailRepository,
+    findUserByIdRepository,
+    findAllUserRepository,
+    updateUserRepository,
+    deleteUserRepository
 }
---
+++ src/schema/user.schema.js
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const userSchema = z.object({
+    username:z.string().min(3,"Username is required"),
+    email: z.string().email("Invalid email"), 
+    password: z.string().min(6, "Password must be at least 6 characteres long"),
+    avatar: z.string().url("Invalid URL").optional(),
+
+})
+
+const userIdSchema = z.object({
+    userId: z.number().int().positive("User ID must be a positive integer"),
+});
+
+export { userSchema, userIdSchema };
